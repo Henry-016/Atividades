988b0d1ca71b7314b0ab12998ebacd3ee368060f
Henry-016 <enriquepereiraleite@gmail.com>
2026-01-13 14:59:03 -0300

    Add files via upload

---
+++ Atividades_Da_Faculdade/AtividadeDePOO - PadrõesDeProjeto/AtividadeDePOO - Padrões de Projeto/Interfaces/ICarta.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace AtividadeDePOO___Padrões_de_Projeto.Interfaces
+{
+    public interface ICarta
+    {
+        void GerarCarta(string texto, string destinatario);
+    }
+}
---
+++ Atividades_Da_Faculdade/AtividadeDePOO - PadrõesDeProjeto/AtividadeDePOO - Padrões de Projeto/Interfaces/IDocumentos.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace AtividadeDePOO___Padrões_de_Projeto.Interfaces
+{
+    public interface IDocumentos
+    {
+
+    }
+}
