e6270e7a24b7e894cd0be5d80b390846a68a5bdb
Henry-016 <enriquepereiraleite@gmail.com>
2026-01-13 14:54:20 -0300

    Delete Atividades_Da_Faculdade/ProjetoDePOO - PadrõesDeProjeto directory

--- Atividades_Da_Faculdade/ProjetoDePOO - PadrõesDeProjeto/AtividadeDePOO - Padrões de Projeto/Interfaces/ICarta.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace AtividadeDePOO___Padrões_de_Projeto.Interfaces
-{
-    public interface ICarta
-    {
-        void GerarCarta(string texto, string destinatario);
-    }
-}
--- Atividades_Da_Faculdade/ProjetoDePOO - PadrõesDeProjeto/AtividadeDePOO - Padrões de Projeto/Interfaces/IRelatorio.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace AtividadeDePOO___Padrões_de_Projeto.Interfaces
-{
-    public interface IRelatorio
-    {
-        void GerarRelatorio();
-    }
-}
